5d99cd399e34bb95ac222575c4fee9c4ca5b1873
Mike Kelly <kelly.mike@abc.net.au>
2024-09-26 13:02:03 +1000

    fix: Fixing the create modal and response

--- api/location/new.go
+++ api/location/new.go
@@ -4,13 +4,21 @@ import (
 	"github.com/labstack/echo/v4"
 
 	"scouts-risk/internal/fixtures"
+	"scouts-risk/internal/model"
 	web_location "scouts-risk/web/location"
 )
 
 func New(c echo.Context) error {
 	newLabel := c.FormValue("label")
+	newType := c.FormValue("type")
+	newTemplate := c.FormValue("template")
+
 	newArea := fixtures.LocationArea
 	newArea.Label = newLabel
+	newArea.LocType = model.LocationType(newType)
+	if (newTemplate == "") {
+		newArea.Hazards = []model.Hazard{}
+	}
 
 	html := web_location.Review(newArea)
 	return html.Render(c.Request().Context(), c.Response().Writer)
